35ce7f14eb7960fb6f9cf6e3bd2b20310291b6ec
nathanfaucett <nathanfaucett@gmail.com>
2015-10-18 15:57:40 -0500

    updates

---
+++ test/index.js
@@ -0,0 +1,22 @@
+var tape = require("tape"),
+    inflections = require("inflections");
+
+
+require("..");
+
+
+tape("inflections_es", function(assert) {
+    var inflector = inflections("es");
+
+    assert.equal(inflector.singularize("libros", "es"), "libro");
+    assert.equal(inflector.singularize("radios", "es"), "radio");
+    assert.equal(inflector.singularize("señores", "es"), "señor");
+    assert.equal(inflector.singularize("leyes", "es"), "ley");
+
+    assert.equal(inflector.pluralize("libro", "es"), "libros");
+    assert.equal(inflector.pluralize("radio", "es"), "radios");
+    assert.equal(inflector.pluralize("señor", "es"), "señores");
+    assert.equal(inflector.pluralize("ley", "es"), "leyes");
+
+    assert.end();
+});
